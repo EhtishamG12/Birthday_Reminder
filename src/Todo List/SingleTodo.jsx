82d924279396f731960d17326d7261bd5ce4ba9b
EhtishamG12 <ehtishama975@gmail.com>
2024-07-11 18:29:49 +0500

    This is react project

--- src/Todo List/SingleTodo.jsx
+++ src/Todo List/SingleTodo.jsx
@@ -1,3 +1,18 @@
 import React, {useState} from "react";
 import SingleTodo from "./SingleTodo";
 import toast from "react-hot-toast";
+
+const Todo = () => {
+  const [todo,setTodo] =useState("");
+  const [error,setError] = useState(false);
+
+  const [data,setData] = useState([]);
+
+  const handletodo = (e) => {
+ e.preventDefault()
+
+
+  }
+
+
+};
